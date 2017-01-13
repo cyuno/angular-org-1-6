ec0bd5cb94178b7d5f613b181dcaf305c050a880
cyuno <cy.unpingco@gmail.com>
2017-01-12 16:39:47 -0800

    Add files via upload

---
+++ angular-org-1-6/example3/project-list.js
@@ -0,0 +1,97 @@
+angular.module('project').value('projectListValue', [
+  {
+    name: 'AngularJS',
+    site: 'http://angularjs.org',
+    description: 'HTML enhanced for web apps!'
+  },
+  {
+    name: 'Angular 2',
+    site: 'http://angular.io',
+    description: 'One framework. Mobile and desktop.'
+  },
+  {
+    name: 'jQuery',
+    site: 'http://jquery.com/',
+    description: 'Write less, do more.'
+  },
+  {
+    name: 'Backbone',
+    site: 'http://backbonejs.org/',
+    description: 'Models for your apps.'
+  },
+  {
+    name: 'SproutCore',
+    site: 'http://sproutcore.com/',
+    description: 'A Framework for Innovative web-apps.'
+  },
+  {
+    name: 'Polymer',
+    site: 'https://www.polymer-project.org/',
+    description: 'Reusable components for the modern web.'
+  },
+  {
+    name: 'Spine',
+    site: 'http://spinejs.com/',
+    description: 'Awesome MVC Apps.'
+  },
+  {
+    name: 'Cappucino',
+    site: 'http://www.cappuccino-project.org/',
+    description: 'Objective-J.'
+  },
+  {
+    name: 'Knockout',
+    site: 'http://knockoutjs.com/',
+    description: 'MVVM pattern.'
+  },
+  {
+    name: 'GWT',
+    site: 'http://www.gwtproject.org/',
+    description: 'JS in Java.'
+  },
+  {
+    name: 'Ember',
+    site: 'http://emberjs.com/',
+    description: 'Ambitious web apps.'
+  },
+  {
+    name: 'React',
+    site: 'https://facebook.github.io/react/',
+    description: 'A JavaScript library for building user interfaces.'
+  },
+  {
+    name: 'Dart',
+	site: 'https://www.dartlang.org',
+	description: 'An application programming language.'
+  },
+  {
+    name: 'Sencha',
+	site: 'https://www.sencha.com',
+	description:'Rapidly build data-intensive web apps'
+  },
+  {
+    name: 'Java',
+	site: 'https://www.oracle.com/java/index.html',
+	description: 'Java is the world\'s number one programming language.'
+   },
+   {
+     name:'Python',
+	 site: 'https://www.python.org/',
+	 description: 'Lets you work quickly'
+   },
+   {
+     name:'PHP',
+	 site:'http://php.net/',
+	 description:'Popular general-purpose scripting language'
+   },
+   {
+     name:'C#',
+	 site:'https://msdn.microsoft.com/en-us/library/kx37x362.aspx',
+	 description:'Programming language run on the .NET Framework'
+   },
+   {
+     name:'Ruby',
+	 site:'https://www.ruby-lang.org/en/',
+	 description:'Programming language with a focus on simplicity and productivity.'
+    }
+])
